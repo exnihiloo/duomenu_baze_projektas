c1abaf673e256e6561b56ddc7e64229ced5185c6
exnihiloo <deimante.jasaityte@gmail.com>
2022-10-24 09:05:49 +0300

    created class "sale" with table "sale"

--- teatras_modelis.py
+++ teatras_modelis.py
@@ -5,3 +5,15 @@ from sqlalchemy.orm import relationship
 
 engine = create_engine('sqlite:///data/teatras.db')
 Base = declarative_base()
+
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+    id = Column(Integer, primary_key = True)
+    pavadinimas = Column('Sales pavadinimas', String)
+    spektakliai = relationship("Spektaklis", back_populates = 'sale')
+
+
+    def __repr__(self):
+        return f"{self.id}) {self.pavadinimas}."
